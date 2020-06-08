0c93f7f0f81e2db4abbee1cdd9ef71aee88c2fd0
thomasRita <thomasrita98@yahoo.com>
2020-06-08 15:13:52 +0100

    add post route

--- index.js
+++ index.js
@@ -5,15 +5,19 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 
-const get = require();
-const post = require();
-const update = require();
-const remove = require();
+const get = require('./routes/get');
+const post = require('./routes/post');
+// const update = require();
+// const remove = require();
 
 app.use('api/v1/', get);
 app.use('api/v1/upload', post);
-app.use('api/v1/:id', update);
-app.use('api/v1/:id', remove);
+// app.use('api/v1/:id', update);
+// app.use('api/v1/:id', remove);
+
+app.get('/', (req, res) => res.status(200).json({
+    message: 'Welcome!'
+  }));
 
 const port = (process.env.PORT || 3000);
 app.listen(port, ()=> `Server now running on ${port}`)
